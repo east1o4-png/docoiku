b7b31805bcd59fce57b6443792643a4869fd3ad8
east1o4-png <east1o4@icloud.com>
2026-07-25 06:33:42 +0900

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -6,9 +6,22 @@ const config = {
   type: Phaser.AUTO,
   parent: "game",
   backgroundColor: "#7fcde8",
-  scale: { mode: Phaser.Scale.RESIZE, autoCenter: Phaser.Scale.CENTER_BOTH, width: window.innerWidth, height: window.innerHeight },
-  render: { antialias: true, pixelArt: false, roundPixels: false },
-  physics: { default: "arcade", arcade: { debug: false } },
+  scale: {
+    mode: Phaser.Scale.RESIZE,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    width: window.innerWidth,
+    height: window.innerHeight
+  },
+  render: {
+    antialias: true,
+    pixelArt: false,
+    roundPixels: false
+  },
+  physics: {
+    default: "arcade",
+    arcade: { debug: false }
+  },
   scene: [BootScene, HomeScene, IslandScene]
 };
+
 new Phaser.Game(config);
