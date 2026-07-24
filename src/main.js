0003151e34c5ed8a0f44f9e878df1b63d6811c2a
east1o4-png <east1o4@icloud.com>
2026-07-24 23:13:57 +0900

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -5,33 +5,10 @@ import { IslandScene } from "./scenes/IslandScene.js";
 const config = {
   type: Phaser.AUTO,
   parent: "game",
-  backgroundColor: "#89cfe8",
-
-  scale: {
-    mode: Phaser.Scale.RESIZE,
-    autoCenter: Phaser.Scale.CENTER_BOTH,
-    width: window.innerWidth,
-    height: window.innerHeight
-  },
-
-  render: {
-    antialias: true,
-    pixelArt: false,
-    roundPixels: false
-  },
-
-  physics: {
-    default: "arcade",
-    arcade: {
-      debug: false
-    }
-  },
-
-  scene: [
-    BootScene,
-    HomeScene,
-    IslandScene
-  ]
+  backgroundColor: "#7fcde8",
+  scale: { mode: Phaser.Scale.RESIZE, autoCenter: Phaser.Scale.CENTER_BOTH, width: window.innerWidth, height: window.innerHeight },
+  render: { antialias: true, pixelArt: false, roundPixels: false },
+  physics: { default: "arcade", arcade: { debug: false } },
+  scene: [BootScene, HomeScene, IslandScene]
 };
-
 new Phaser.Game(config);
