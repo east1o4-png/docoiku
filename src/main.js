2b5f19edcd8558f527c9124fb038a6bdaf50b5c5
east1o4-png <east1o4@icloud.com>
2026-07-24 21:57:34 +0900

    Add physics configuration to Phaser game

--- src/main.js
+++ src/main.js
@@ -6,18 +6,32 @@ const config = {
   type: Phaser.AUTO,
   parent: "game",
   backgroundColor: "#89cfe8",
+
   scale: {
     mode: Phaser.Scale.RESIZE,
     autoCenter: Phaser.Scale.CENTER_BOTH,
     width: window.innerWidth,
     height: window.innerHeight
   },
+
   render: {
     antialias: true,
     pixelArt: false,
     roundPixels: false
   },
-  scene: [BootScene, HomeScene, IslandScene]
+
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: false
+    }
+  },
+
+  scene: [
+    BootScene,
+    HomeScene,
+    IslandScene
+  ]
 };
 
 new Phaser.Game(config);
